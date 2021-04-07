64da407f518651c0280ea9d617a8d5c3957a6141
jbutle55 <justin.butler12@hotmail.ca>
2021-04-07 14:04:11 -0600

    ROC Curve Work

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -22,6 +22,7 @@ import urllib.request
 import shutil
 import warnings
 from distutils.version import LooseVersion
+from matplotlib import pyplot as plt
 
 # URL from which to download the latest COCO trained weights
 COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
@@ -874,52 +875,126 @@ def compute_ap_range(gt_box, gt_class_id, gt_mask,
     return AP
 
 
-def compute_roc_curve():
+def compute_roc_curve(tp_rates, fp_rates, confidence_thresholds):
     """
 
     """
+
+    plt.plot(fp_rates, tp_rates)
+    plt.xlabel('False Positive Rates')
+    plt.ylabel('True Positive Rates')
+    plt.title('ROC Curve')
+    plt.show()
 
     return
 
 
-def compute_tpr_fpr(gt_boxes, gt_class_ids, gt_masks,
-               pred_boxes, pred_class_ids, pred_scores, pred_masks,
-               iou_threshold=0.5):
+def compute_fpr_indiv_class(gt_boxes, gt_class_ids, gt_masks,
+                                pred_boxes, pred_class_ids, pred_scores, pred_masks, filter_class,
+                                iou_threshold=0.5, score_threshold=0.0):
+    total_fpr = {}
+    classes = list(set(gt_class_ids))  # All unique class ids from gts
+
+    # Find True Negative or each class
+    for ind_class in classes:
+
+        # Get matches and overlaps
+        # gt_match has index of matched pred box
+        tn_count, fp_count = compute_tn_fp_indiv_class(
+            gt_boxes, gt_class_ids, gt_masks,
+            pred_boxes, pred_class_ids, pred_scores, pred_masks, ind_class,
+            iou_threshold)
+
+        fpr = fp_count / (fp_count + tn_count)
+
+        total_fpr[ind_class] = fpr
+
+    return total_fpr
+
+
+def compute_tn_fp_indiv_class(gt_boxes, gt_class_ids, gt_masks,
+                                pred_boxes, pred_class_ids, pred_scores, pred_masks, filter_class,
+                                iou_threshold=0.5, score_threshold=0.0):
     """
-    Compute the True Positive and  False Positive Rates
+
+    Returns:
+
     """
+    # Trim zero padding
+    # TODO: cleaner to do zero unpadding upstream
+    gt_boxes = trim_zeros(gt_boxes)
+    gt_masks = gt_masks[..., :gt_boxes.shape[0]]
+    pred_boxes = trim_zeros(pred_boxes)
+    pred_scores = pred_scores[:pred_boxes.shape[0]]
+    # Sort predictions by score from high to low
+    indices = np.argsort(pred_scores)[::-1]
+    pred_boxes = pred_boxes[indices]
+    pred_class_ids = pred_class_ids[indices]
+    pred_scores = pred_scores[indices]
+    pred_masks = pred_masks[..., indices]
 
-    tn, fp = [], []
+    # Compute IoU overlaps [pred_masks, gt_masks]
+    overlaps = compute_overlaps_masks(pred_masks, gt_masks)
 
-    # Only want to compute tpr and fpr for a single class at a time
+    # Loop through predictions and find matching ground truth boxes
+    # If there's a match, remove from both pred and GT lists
+    fp_count = 0
 
+    match_count = 0
+    pred_match = -1 * np.ones([pred_boxes.shape[0]])
+    gt_match = -1 * np.ones([gt_boxes.shape[0]])
+    for i in range(len(pred_boxes)):
+        # Find best matching ground truth box
+        # 1. Sort matches by score
+        sorted_ixs = np.argsort(overlaps[i])[::-1]
+        # 2. Remove low scores
+        low_score_idx = np.where(overlaps[i, sorted_ixs] < score_threshold)[0]
+        if low_score_idx.size > 0:
+            sorted_ixs = sorted_ixs[:low_score_idx[0]]
+        # 3. Find the match
+        print(f'Starting Pred Length - {len(pred_class_ids)}')
+        for j in sorted_ixs:
+            # Skip preds not of filter class
+            #if pred_class_ids[i] != filter_class:
+            #    # True Negative or False Negative
+            #    continue
+            # If ground truth box is already matched, go to next one
+            #if gt_match[j] > -1:
+            #    continue
+            # If we reach IoU smaller than the threshold, end the loop
+            iou = overlaps[i, j]
+            if iou < iou_threshold:
+                break
+            # Do we have a match?
+            # True Positive
+            if pred_class_ids[i] == gt_class_ids[j]:
+                # Remove True Positives from list
+                pred_class_ids.pop(i)
 
-    # Get matches and overlaps
-    gt_match, pred_match, overlaps = compute_matches(
-        gt_boxes, gt_class_ids, gt_masks,
-        pred_boxes, pred_class_ids, pred_scores, pred_masks,
-        iou_threshold)
+                #match_count += 1
+                #gt_match[j] = i
+                #pred_match[i] = j
+                break
+            # False Positive
+            elif pred_class_ids[i] != gt_class_ids[j]:
+                if gt_class_ids[j] == filter_class:
+                    # Remove False Negatives
+                    pred_class_ids.pop(i)
+                    break
+                else:
+                    fp_count += 1
+                    # Remove False Positives
+                    pred_class_ids.pop(i)
+                    break
 
-    # Compute precision and recall at each prediction box step
-    precisions = np.cumsum(pred_match > -1) / (np.arange(len(pred_match)) + 1)
-    recalls = np.cumsum(pred_match > -1).astype(np.float32) / len(gt_match)  # True Positives
+    # Now have list of preds that are  TN
+    tn_count = len(pred_class_ids)
 
-    # Pad with start and end values to simplify the math
-    precisions = np.concatenate([[0], precisions, [0]])
-    recalls = np.concatenate([[0], recalls, [1]])
+    print('DEBUG PRINT')
+    print(f'TN Count - {tn_count}')
+    print(f'FP Count - {fp_count}')
 
-    # Ensure precision values decrease but don't increase. This way, the
-    # precision value at each recall threshold is the maximum it can be
-    # for all following recall thresholds, as specified by the VOC paper.
-    for i in range(len(precisions) - 2, -1, -1):
-        precisions[i] = np.maximum(precisions[i], precisions[i + 1])
-
-    # Compute mean AP over recall range
-    indices = np.where(recalls[:-1] != recalls[1:])[0] + 1
-    mAP = np.sum((recalls[indices] - recalls[indices - 1]) *
-                 precisions[indices])
-
-    return tn, fp
+    return tn_count, fp_count
 
 
 def compute_recall(pred_boxes, gt_boxes, iou):
