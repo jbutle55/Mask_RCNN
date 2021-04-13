a0ad619a847bf51d179ae303691d0dcbc426b507
jbutle55 <justin.butler12@hotmail.ca>
2021-04-13 14:31:27 -0600

    Fix bug, change wescam config

--- run_mask.py
+++ run_mask.py
@@ -6,6 +6,8 @@ import cv2
 import datetime
 import skimage
 import imgaug
+import random
+import math
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -199,7 +201,7 @@ class ShapesDataset(utils.Dataset):
 
 class WESCAMConfig(Config):
     # Give the configuration a recognizable name
-    NAME = "shapes"
+    NAME = "wescam"
 
     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
@@ -207,25 +209,25 @@ class WESCAMConfig(Config):
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 3  # background + 3 shapes
+    # NUM_CLASSES = 1 + 1  # background + 3 shapes
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 512
+    #IMAGE_MIN_DIM = 1024
+    #IMAGE_MAX_DIM = 1024
 
     # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    #RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 32
+    #TRAIN_ROIS_PER_IMAGE = 32
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 300
+    #STEPS_PER_EPOCH = 300
 
     # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 5
+    #VALIDATION_STEPS = 5
 
     USE_MINI_MASK = False
 
@@ -296,7 +298,7 @@ def main(args):
 
     # Train or evaluate
     if command == "train":
-        if config == 'coco:'
+        if config == 'coco':
             # Training dataset. Use the training set and 35K from the
             # validation set, as as in the Mask RCNN paper.
             dataset_train = CocoDataset()
