b6fab07ae1fc29750cefaedc56a053553b3b71a4
jbutle55 <justin.butler12@hotmail.ca>
2021-04-13 16:25:31 -0600

    Add debug print

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -457,6 +457,9 @@ def resize_image(image, min_dim=None, max_dim=None, min_scale=None, mode="square
         left_pad = (max_dim - w) // 2
         right_pad = max_dim - w - left_pad
         padding = [(top_pad, bottom_pad), (left_pad, right_pad), (0, 0)]
+
+        print(f'padding - {padding}')
+
         image = np.pad(image, padding, mode='constant', constant_values=0)
         window = (top_pad, left_pad, h + top_pad, w + left_pad)
     elif mode == "pad64":
