fa02611556c236f8a8b42c13ef5aa04914338f6c
jbutle55 <justin.butler12@hotmail.ca>
2021-04-15 13:43:08 -0600

    change weight loading to skip last layer

--- run_mask.py
+++ run_mask.py
@@ -717,7 +717,10 @@ def main(args):
 
     # Load weights
     print("Loading weights ", model_path)
-    model.load_weights(model_path, by_name=True)
+    model.load_weights(model_path, by_name=True, exclude=[
+        "mrcnn_class_logits", "mrcnn_bbox_fc",
+        "mrcnn_bbox", "mrcnn_mask"])
+    #model.load_weights(model_path, by_name=True)
 
     model.keras_model.summary()
 
