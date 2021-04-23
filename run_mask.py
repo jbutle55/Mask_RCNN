6b2bbb97f67c8aceaf4e8127027f4010d93413b8
jbutle55 <justin.butler12@hotmail.ca>
2021-04-23 13:26:37 -0600

    Increase STEPS_PER_EPOCH. Use OG anchors

--- run_mask.py
+++ run_mask.py
@@ -790,7 +790,7 @@ class AerialConfig(Config):
     IMAGE_MAX_DIM = 1024
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 200
 
     # use small validation steps since the epoch is small
     VALIDATION_STEPS = 50
@@ -801,8 +801,8 @@ class AerialConfig(Config):
     DETECTION_MIN_CONFIDENCE = 0.5
 
     # Length of square anchor side in pixels
-    # RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
-    RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
+    RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
+    #RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
 
     # Non-max suppression threshold to filter RPN proposals.
     # You can increase this during training to generate more propsals.
